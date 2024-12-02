048cbb7854e984b76de938bd01b443f804dbd06c
Richard Ivánek <richard@ivanek.dev>
2024-12-02 22:44:56 +0100

    feat(rust): day 1

---
+++ rust/day01/src/main.rs
@@ -0,0 +1,52 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use anyhow::bail;
+use aoc_utils::MinHeap;
+
+fn main() -> anyhow::Result<()> {
+    let reader = BufReader::new(File::open("data.txt")?);
+
+    let mut left_list = MinHeap::<usize>::new();
+    let mut right_list = MinHeap::<usize>::new();
+
+    let mut left_items = HashSet::new();
+    let mut right_occurrences = HashMap::<usize, usize>::new();
+
+    for line in reader.lines() {
+        let line = line?;
+
+        let mut split = line.split("   ");
+
+        let [Some(left), Some(right)] =
+            std::array::from_fn(|_| split.next().and_then(|v| v.parse().ok()))
+        else {
+            bail!("failed to parse input")
+        };
+
+        left_list.push(left);
+        right_list.push(right);
+
+        left_items.insert(left);
+        *right_occurrences.entry(right).or_default() += 1;
+    }
+
+    let result = std::iter::from_fn(|| left_list.pop())
+        .zip(std::iter::from_fn(|| right_list.pop()))
+        .map(|(l, r)| l.abs_diff(r))
+        .sum::<usize>();
+
+    println!("the list diff is {result}");
+
+    let similarity = left_items
+        .iter()
+        .map(|i| right_occurrences.get(i).copied().unwrap_or_default() * *i)
+        .sum::<usize>();
+
+    println!("similarity score {similarity}");
+
+    Ok(())
+}
