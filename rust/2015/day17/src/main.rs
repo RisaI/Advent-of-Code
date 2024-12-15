503dbb7f06a91a08771389b9ffcaaec35b378482
Richard Ivánek <richard@ivanek.dev>
2024-12-15 16:06:26 +0100

    feat(rust): 2015, day 17

---
+++ rust/2015/day17/src/main.rs
@@ -0,0 +1,38 @@
+use anyhow::Context;
+
+fn partitions(value: usize, containers: &[usize], budget: usize) -> usize {
+    if containers.is_empty() || budget == 0 {
+        return 0;
+    }
+
+    (match containers[0].cmp(&value) {
+        std::cmp::Ordering::Greater => 0,
+        std::cmp::Ordering::Equal => 1,
+        std::cmp::Ordering::Less => partitions(value - containers[0], &containers[1..], budget - 1),
+    }) + partitions(value, &containers[1..], budget)
+}
+
+fn main() -> anyhow::Result<()> {
+    let containers = std::fs::read_to_string("data.txt")?
+        .split('\n')
+        .filter(|v| !v.is_empty())
+        .map(|v| v.parse::<usize>())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    println!(
+        "{} partitions",
+        partitions(150, &containers, containers.len())
+    );
+
+    let (num_containers, combinations) = (1..containers.len())
+        .map(|c| (c, partitions(150, &containers, c)))
+        .find(|(_, v)| *v > 0)
+        .context("no solution for p2 found, something must be wrong")?;
+
+    println!(
+        "{} partitions with {} containers",
+        combinations, num_containers
+    );
+
+    Ok(())
+}
--- rust/create-day.fish
+++ rust/create-day.fish
@@ -3,10 +3,13 @@
 set day_idx (math (count day??) + 1)
 set day_name day(string pad --char='0' -w 2 $day_idx)
 
-echo https://adventofcode.com/2024/day/$day_idx/input
-
 if not test -e $day_name
     cargo new $day_name --name aoc24-$day_name
     cd $day_name
     cargo add anyhow
 end
+
+echo https://adventofcode.com/2024/day/$day_idx
+echo https://adventofcode.com/2024/day/$day_idx/input
+
+cd $day_name
